93edcb308b970bc75b1c8da49a4aedb2dc52fa20
sshagit <39990003+sshagit@users.noreply.github.com>
2025-07-31 07:00:42 +0530

    Delete run_web_ui.bat

--- run_web_ui.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting 56 Card Game Web Interface...
-echo.
-echo Open your browser and go to: http://localhost:5000
-echo Press Ctrl+C to stop the server
-echo.
-cd /d "C:\Users\sshastri\Projects\14.56"
-python simple_web.py
-pause
